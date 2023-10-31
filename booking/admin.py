8467c180510a41ba12bab9fc7353914c21bb30d1
Majdi-Aref <>
2023-10-31 15:41:05 +0000

    feat: create TableAdmin and BookingAdmin classes in the admin.py file

--- booking/admin.py
+++ booking/admin.py
@@ -3,12 +3,17 @@ from .models import Table, Booking, MenuItem
 from django_summernote.admin import SummernoteModelAdmin
 
 
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('number', 'capacity')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'table', 'date', 'time', 'guests')
+
+
 @admin.register(MenuItem)
 class MenuItemAdmin(SummernoteModelAdmin):
-
     list_display = ('name', 'price')
     summernote_fields = ('description',)
-
-
-admin.site.register(Table)
-admin.site.register(Booking)
