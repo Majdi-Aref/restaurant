9b999f098fb24031dc9cca15c82952b408eb26c0
Majdi-Aref <>
2023-10-31 13:22:49 +0000

    feat: create MenuItemAdmin class in admin.py

--- booking/admin.py
+++ booking/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Table, Booking, MenuItem
 from django_summernote.admin import SummernoteModelAdmin
 
+
+@admin.register(MenuItem)
+class MenuItemAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description')
+
+
 admin.site.register(Table)
 admin.site.register(Booking)
-admin.site.register(MenuItem)
