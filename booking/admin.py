ad987b3e8a298f69136b3470f63ef2611da6bae3
Majdi-Aref <>
2023-10-31 13:18:10 +0000

    feat: install Summernote and add it to admin.py, settings.py, and urls.py

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Table, Booking, MenuItem
+from django_summernote.admin import SummernoteModelAdmin
 
 admin.site.register(Table)
 admin.site.register(Booking)
