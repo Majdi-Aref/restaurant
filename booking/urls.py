c2866183ef5b97c67bb8296a006c85f1c10f2f00
Majdi-Aref <>
2023-11-05 00:07:19 +0000

    feat: create menu.html and moving the templates directory into the Django app booking

--- booking/urls.py
+++ booking/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # other paths...
+    path('menu/', views.menu, name='menu'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,11 @@
+from .models import MenuItem
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'base.html')
+
+
+def menu(request):
+    menu_items = MenuItem.objects.all()
+    return render(request, 'menu.html', {'menu_items': menu_items})
