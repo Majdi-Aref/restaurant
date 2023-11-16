6642e43d1f1b2ae1971c56d77ac449099bd71750
Majdi-Aref <>
2023-11-16 02:14:42 +0000

    feat: restrict the booking times options that are available for a customer in forms.py  and including {{ form.time }} in the time field of book.html

--- booking/forms.py
+++ booking/forms.py
@@ -20,7 +20,17 @@ class NewUserForm(UserCreationForm):
         return user
 
 
+class ChoiceTimeField(forms.TimeField):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.widget = forms.Select(
+            choices=[('%02d:00' % i, '%02d:00' % i) for i in [10, 12, 14, 16, 18]])
+
+
 class BookingForm(forms.ModelForm):
+    time = ChoiceTimeField()
+
     class Meta:
         model = Booking
         fields = ['table', 'date', 'time', 'guests']
@@ -33,6 +43,6 @@ class BookingForm(forms.ModelForm):
         if table and guests:
             if guests <= 0 or guests > table.capacity:
                 raise ValidationError(
-                    'You have entered an invalid number of guests; please enter a number of guests that is greater than zero, equal to, or less than the capacity of the table you want to book!')
+                    'You have entered an invalid number of guests; please enter a number of guests that is not zero, equal to, or less than the capacity of the table that you want to book!')
 
         return cleaned_data
