945addbf83789657463a607a03e3cc247cd0a52e
Majdi-Aref <>
2023-11-16 01:01:24 +0000

    feat: add a clean method to the BookingForm in forms.py to validate the number of guests against the capacity of a table and add code to book.html that displays non-field errors

--- booking/forms.py
+++ booking/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Booking
+from django.core.exceptions import ValidationError
 
 
 class NewUserForm(UserCreationForm):
@@ -23,3 +24,15 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['table', 'date', 'time', 'guests']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        table = cleaned_data.get('table')
+        guests = cleaned_data.get('guests')
+
+        if table and guests:
+            if guests <= 0 or guests > table.capacity:
+                raise ValidationError(
+                    'You have entered an invalid number of guests; please enter a number of guests that is greater than zero, equal to, or less than the capacity of the table you want to book!')
+
+        return cleaned_data
